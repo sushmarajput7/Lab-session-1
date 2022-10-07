9658e2dbb51f2e0b8593a44a14e7edd35c561f70
Sushma <114230848+sushmarajput7@users.noreply.github.com>
2022-10-07 13:37:27 +0530

    Add files via upload

--- 6-10-22/Checkpassword.java
+++ 6-10-22/Checkpassword.java
@@ -1,10 +1,31 @@
+/* WAP to check the condition for checking the correct password using If-Else.Take input from the user.
+Author-Sushma
+Date-6/09/2022
+*/
 package Corejava;
+
+import java.util.Scanner;
 
 public class Checkpassword {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the User_Name : ");
+		//we use string for taking input from the user
+		String name = sc.nextLine();
+		System.out.println("Enter your Password : ");
+		int password=sc.nextInt();	
+		//if condition to check if the password is correct or not
+		if(password==123456)
+		{
+		System.out.println(name +" Login Successful..");	
+		}
+		else
+		System.out.println(name +" your password is incorrect..Try Again");	
+		}
+		
 
 }
+
+
